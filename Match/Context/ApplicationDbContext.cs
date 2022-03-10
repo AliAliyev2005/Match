34fc9ff8b73263837374ae42dd41478da2ed72df
Əliyev Əli Ramil <Xliy_g8zy@itstep.edu.az>
2022-03-10 21:23:56 +0400

    initial commit

---
+++ Match/Context/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Match.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Match.Context
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<MatchModel> MatchModels { get; set; }
+    }
+}
---
+++ Match/Entities/MatchModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Match.Entities
+{    
+    public class MatchModel
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public int FirstTeamId { get; set; }
+        public Team FirstTeam { get; set; }
+        public int SecondTeamId { get; set; }
+        public Team SecondTeam { get; set; }
+        public int FirstScore { get; set; }
+        public int SecondScore { get; set; }
+    }
+}
