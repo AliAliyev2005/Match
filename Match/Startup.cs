6692f54b1b23ee7537d0a6480b751bbe2ed84274
Əliyev Əli Ramil <Xliy_g8zy@itstep.edu.az>
2022-03-10 21:41:09 +0400

    commit for pull

--- Match/Startup.cs
+++ Match/Startup.cs
@@ -24,6 +24,7 @@ namespace Match
         public void ConfigureServices(IServiceCollection services)
         {
             //services.AddIdentity<ApplicationDbContext, >();
+            services.AddDbContext<ApplicationDbContext>();
             services.AddControllersWithViews();
         }
 
