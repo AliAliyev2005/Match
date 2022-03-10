0e539f898ddc93fb406dbc755b09a4138859524a
Osmanli Resul Ferhad <Osma_th97@itstep.edu.az>
2022-03-10 21:35:34 +0400

    Added Identity

--- Match/Startup.cs
+++ Match/Startup.cs
@@ -1,6 +1,9 @@
 using Match.Context;
+using Match.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,7 +26,13 @@ namespace Match
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddIdentity<ApplicationDbContext, >();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                    .AddEntityFrameworkStores<ApplicationDbContext>()
+                    .AddDefaultTokenProviders();
+
             services.AddControllersWithViews();
         }
 
